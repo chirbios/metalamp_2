2560ff9ecb1793022b3768a6158b9bed5bb2020f
chirbios <kazzon95@bk.ru>
2022-02-24 00:04:55 +0300

    changes in dropdown

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -2672,7 +2672,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54525" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54765" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
